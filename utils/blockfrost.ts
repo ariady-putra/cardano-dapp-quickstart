dd72509ffe106032be9f64bdff9dddcfb08d9155
Ariady Putra <ariady.putra@gmail.com>
2023-06-19 12:14:51 +0700

    Use PosixTime from BF instead of new Date()

--- utils/blockfrost.ts
+++ utils/blockfrost.ts
@@ -1,3 +1,18 @@
+export async function getLatestBlockInfo() {
+  const block = await fetch(
+    "https://cardano-preview.blockfrost.io/api/v0/blocks/latest",
+    {
+      headers: {
+        project_id: process.env.NEXT_PUBLIC_BLOCKFROST as string,
+      },
+    }
+  );
+  const blockJson = await block.json();
+  // console.log({ blockJson: blockJson });
+
+  return blockJson;
+}
+
 export async function getDatumFields(datumHash: string) {
   const datum = await fetch(
     `https://cardano-preview.blockfrost.io/api/v0/scripts/datum/${datumHash}`,
